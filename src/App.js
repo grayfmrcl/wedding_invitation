7a068c27546358e2057633f4b7c89b0960943512
grayfmrcl <grayfieldmm@gmail.com>
2024-01-01 10:52:47 +0700

    hide firebase config

--- src/App.js
+++ src/App.js
@@ -16,19 +16,18 @@ import { useEffect, useState } from 'react';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAUhtb516LEjfGYSYsv_AYp71grUH_sbLk",
-  authDomain: "wedding-invitation-e055a.firebaseapp.com",
-  databaseURL: "https://wedding-invitation-e055a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wedding-invitation-e055a",
-  storageBucket: "wedding-invitation-e055a.appspot.com",
-  messagingSenderId: "342807744917",
-  appId: "1:342807744917:web:c59717a4df9b4f495e669c",
-  measurementId: "G-8CP8BGJEDY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
 
 const db = getDatabase();
 
