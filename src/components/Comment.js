31378f489eaffe0ebc4c16cbb3f92c531f96039d
grayfmrcl <grayfieldmm@gmail.com>
2024-01-09 10:54:23 +0700

    adjust styling and icon in Comment and Reservation

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function Comment({ title, comment }) {
-  return <div style={{ fontFamily: 'Quicksand', color: '#FFF5EA', padding: '1vh' }}>
-    <p><b>{comment}</b></p>
-    <span> {title}</span>
-    <hr></hr>
+  return <div style={{ fontFamily: 'Quicksand', color: '#FFF5EA', padding: '2vh' }}>
+    <p>
+      <b>{`"${comment}"`}</b>
+    </p>
+    <span>
+      <i>-{title}-</i>
+    </span>
+    <hr style={{ margin: '0.25rem' }}></hr>
   </div>
 }
 
