9f89270399e2ce1775fd496fd7f13f0df8b7827f
grayfmrcl <grayfieldmm@gmail.com>
2024-01-10 22:33:39 +0700

    remove Maps from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,36 @@
 import React from 'react'
 
-function Navbar () {
+function Navbar() {
   return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid" style={{
-        backgroundColor: '#FFF5EA'}}>
-        <a className="navbar-brand" href="#jumbotron" 
-         style={{
-          color: '#968C83', 
-          fontSize: '3vh',
-          fontFamily : 'Sacramento'
-         }}> <b> GC </b></a>
-        <button className="navbar-toggler" style={{color: '#968C83'}} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        backgroundColor: '#FFF5EA'
+      }}>
+        <a className="navbar-brand" href="#jumbotron"
+          style={{
+            color: '#968C83',
+            fontSize: '3vh',
+            fontFamily: 'Sacramento'
+          }}> <b> GC </b></a>
+        <button className="navbar-toggler" style={{ color: '#968C83' }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse ms-lg-5" id="navbarNav" style={{fontFamily : 'Quicksand'}}>
+        <div className="collapse navbar-collapse ms-lg-5" id="navbarNav" style={{ fontFamily: 'Quicksand' }}>
           <ul className="navbar-nav ms-lg-5">
             <li className="nav-item ms-lg-5">
-              <a className="nav-link active" aria-current="page" href="#jumbotron" style={{color: '#968C83', fontSize: '1.5vh'}}>Home</a>
+              <a className="nav-link active" aria-current="page" href="#jumbotron" style={{ color: '#968C83', fontSize: '1.5vh' }}>Home</a>
             </li>
             <li className="nav-item ms-lg-5">
-              <a className="nav-link" href="#wedding" style={{color: '#968C83', fontSize: '1.5vh'}}>Profile</a>
+              <a className="nav-link" href="#wedding" style={{ color: '#968C83', fontSize: '1.5vh' }}>Profile</a>
             </li>
             <li className="nav-item ms-lg-5">
-              <a className="nav-link" href="#invitation" style={{color: '#968C83', fontSize: '1.5vh'}}>Invitation</a>
+              <a className="nav-link" href="#invitation" style={{ color: '#968C83', fontSize: '1.5vh' }}>Invitation</a>
             </li>
             <li className="nav-item ms-lg-5">
-              <a className="nav-link" href="#gallery" style={{color: '#968C83', fontSize: '1.5vh'}}>Gallery</a>
+              <a className="nav-link" href="#gallery" style={{ color: '#968C83', fontSize: '1.5vh' }}>Gallery</a>
             </li>
             <li className="nav-item ms-lg-5">
-              <a className="nav-link" href="#congratulations" style={{color: '#968C83', fontSize: '1.5vh'}}>Wishes</a>
-            </li>
-            <li className="nav-item ms-lg-5">
-              <a className="nav-link" href="#maps" style={{color: '#968C83', fontSize: '1.5vh'}}>Maps</a>
+              <a className="nav-link" href="#congratulations" style={{ color: '#968C83', fontSize: '1.5vh' }}>Wishes</a>
             </li>
           </ul>
         </div>
